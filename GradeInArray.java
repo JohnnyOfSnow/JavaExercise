521e28dd070dc886484d2a8240e724c3926023a2
JavaNineEditorExecrise6.1 <good258johnny@yahoo.com.tw>
2013-12-10 20:11:06 +0800

    Update GradeInArray.java

--- GradeInArray.java
+++ GradeInArray.java
@@ -8,6 +8,8 @@ import java.util.Scanner;
 public class GradeInArray{
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter the number of students: ");
 		int ArraySize = input.nextInt();
 		int[] scores = new int[ArraySize];
 		
@@ -16,23 +18,23 @@ public class GradeInArray{
 			scores[i] = input.nextInt();
 		}
 		
-		int max = scores[0];
+		int best = scores[0];
 		for (int i = 1; i < ArraySize; i++) {
 			if (scores[i] > max)
-			int best = scores[i];
+			best = scores[i];
 		}
 		
 		for (int i = 0; i < ArraySize; i++) {
 			if (scores[i] >= best - 10){
-				System.out.println("Student " + i + "score is " + scores[i] + "and grade is A");
+				System.out.println("Student " + i + " score is " + scores[i] + " and grade is A");
 			}else if (scores[i] >= best - 20){
-				System.out.println("Student " + i + "score is " + scores[i] + "and grade is B");
+				System.out.println("Student " + i + " score is " + scores[i] + " and grade is B");
 			}else if (scores[i] >= best - 30){
-				System.out.println("Student " + i + "score is " + scores[i] + "and grade is C");
+				System.out.println("Student " + i + " score is " + scores[i] + " and grade is C");
 			}else if (scores[i] >= best - 40){
-				System.out.println("Student " + i + "score is " + scores[i] + "and grade is D");
+				System.out.println("Student " + i + " score is " + scores[i] + " and grade is D");
 			}else{
-				System.out.println("Student " + i + "score is " + scores[i] + "and grade is F");
+				System.out.println("Student " + i + " score is " + scores[i] + " and grade is F");
 			}
 		}
 	}
